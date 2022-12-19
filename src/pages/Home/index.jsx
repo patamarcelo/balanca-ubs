f108afddea672a5664a89355e160825a174d5a36
Marcelo Pata <patamarcelo@gmail.com>
2022-12-19 16:43:05 -0300

    adjust loading

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -58,14 +58,18 @@ const HomePage = () => {
 
 	useEffect(() => {
 		const getData = async () => {
+			setIsLoading(true);
 			const data = await getTruckMoves();
 			console.log("from d: ", data);
 			dispatch(setTruckLoads(data));
+			setIsLoading(false);
 		};
 		if (table.length === 0) {
 			console.log("pegando os dados da Home");
 			getData();
+			setIsLoading(false);
 		}
+		setIsLoading(false);
 	}, []);
 
 	const handleOpenModal = async (obj, data) => {
