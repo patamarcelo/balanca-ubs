e77619638c0cf332840c07f8c93ef6ee59feef70
Marcelo Pata <patamarcelo@gmail.com>
2022-12-25 09:00:41 -0300

    added unidadeOp to reducer select user

--- src/store/user/user.selector.js
+++ src/store/user/user.selector.js
@@ -6,4 +6,6 @@ export const selectIsAdminUser = (state) => state.user.isAdmin;
 
 export const selectIBalancaUser = (state) => state.user.isBalanca;
 
+export const selectUnidadeOpUser = (state) => state.user.unidadeOp;
+
 export const selectAsaasToken = (state) => state.user.asaasToken;
