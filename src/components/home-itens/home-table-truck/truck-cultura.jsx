94bd53a65b1b45cb424acf92664e36737dfb8584
Marcelo Pata <patamarcelo@gmail.com>
2023-01-24 13:16:59 -0300

    dif nowrap cultura

--- src/components/home-itens/home-table-truck/truck-cultura.jsx
+++ src/components/home-itens/home-table-truck/truck-cultura.jsx
@@ -1,10 +1,12 @@
 import { Box, Typography, useTheme } from "@mui/material";
 import { tokens } from "../../../theme";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const CulturaTruck = (props) => {
 	const { data } = props;
 	const theme = useTheme();
 	const colors = tokens(theme.palette.mode);
+	const isNonMobile = useMediaQuery("(min-width: 900px)");
 
 	return (
 		<Box
@@ -12,9 +14,16 @@ const CulturaTruck = (props) => {
 			flexDirection="column"
 			justifyContent="space-around"
 			alignItems="center"
+			sx={{
+				whiteSpace: !isNonMobile && "nowrap"
+			}}
 		>
 			<>
-				<Typography variant="h6" color={colors.primary[300]} sx={{fontStyle: 'italic'}}>
+				<Typography
+					variant="h6"
+					color={colors.primary[300]}
+					sx={{ fontStyle: "italic" }}
+				>
 					Cultura
 				</Typography>
 				<Typography variant="h6" color={colors.primary[100]}>
