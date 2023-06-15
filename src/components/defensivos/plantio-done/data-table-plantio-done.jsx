8466835f386fdd4faafedfb09993d8782f555f44
patamarcelo <patamarcelo@gmail.com>
2023-06-15 15:00:51 -0300

    load data from plantioApi

---
+++ src/components/defensivos/plantio-done/data-table-plantio-done.jsx
@@ -0,0 +1,82 @@
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../../theme";
+
+import CustomToolbar from "../../../utils/format-suport/custom-toolbar";
+
+const PlantioDoneTable = (props) => {
+	const { rows, loading } = props;
+	const theme = useTheme();
+	const colors = tokens(theme.palette.mode);
+
+	const columns = [
+		{ field: "data_plantio", headerName: "Data", flex: 0 },
+		{
+			field: "talhao__fazenda__fazenda__nome",
+			headerName: "Fazenda",
+			flex: 1
+		},
+		{ field: "talhao__fazenda__nome", headerName: "Projeto", flex: 1 },
+		{ field: "talhao__id_talhao", headerName: "Parcela", flex: 1 },
+		{
+			field: "variedade__cultura__cultura",
+			headerName: "Cultura",
+			flex: 1
+		},
+		{
+			field: "variedade__nome_fantasia",
+			headerName: "Variedade",
+			flex: 1
+		},
+		{ field: "area_colheita", headerName: "Area", flex: 1 },
+		{ field: "safra__safra", headerName: "Safra", flex: 1 },
+		{ field: "ciclo__ciclo", headerName: "Ciclo", flex: 1 }
+	];
+
+	return (
+		<Box
+			height="100%"
+			sx={{
+				"& .MuiDataGrid-root": {
+					border: "none"
+				},
+				"& .MuiDataGrid-cell": {
+					// borderBottom: "none"
+				},
+				"& .name-column--cell": {
+					color: colors.pink[300]
+				},
+				"& .MuiDataGrid-columnHeaders": {
+					backgroundColor: colors.blueOrigin[700],
+					borderBottom: "none",
+					height: "90%"
+				},
+				"& .MuiDataGrid-virtualScroller": {
+					backgroundColor: colors.primary[400]
+				},
+				"& .MuiDataGrid-footerContainer": {
+					borderTop: "none",
+					backgroundColor: colors.blueOrigin[700]
+				},
+				"& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+					color: `${colors.grey[100]} !important`
+				},
+				"& .MuiDataGrid-row.Mui-selected": {
+					backgroundColor: `${colors.yellow[900]} !important`
+				}
+			}}
+		>
+			<DataGrid
+				rows={rows}
+				columns={columns}
+				checkboxSelection
+				loading={loading}
+				components={{ Toolbar: CustomToolbar }}
+				componentsProps={{ toolbar: { title: "Plantio" } }}
+			/>
+		</Box>
+	);
+};
+
+export default PlantioDoneTable;
--- src/components/defensivos/plantio-done/index.jsx
+++ src/components/defensivos/plantio-done/index.jsx
@@ -3,14 +3,79 @@ import { tokens } from "../../../theme";
 
 import classes from "./plantio-done-page.module.css";
 
+import { useEffect, useState } from "react";
+
+import djangoApi from "../../../utils/axios/axios.utils";
+import PlantioDoneTable from "./data-table-plantio-done";
+import LoaderHomeSkeleton from "../home/loader";
+
 const PlantioDonePage = () => {
 	const theme = useTheme();
 	const colors = tokens(theme.palette.mode);
+
+	const safraDict = {
+		first: "2022/2023",
+		second: "2023/2024"
+	};
+
+	const cicloDict = {
+		first: "1",
+		second: "2",
+		third: "3"
+	};
+
+	const [dataF, setDataF] = useState([]);
+	const [safra, setSafra] = useState(safraDict.second);
+	const [ciclo, setCiclo] = useState(cicloDict.first);
+
+	const [isLoading, setIsLoading] = useState(true);
+
+	const params = JSON.stringify({
+		safra,
+		ciclo
+	});
+	useEffect(() => {
+		(async () => {
+			try {
+				await djangoApi
+					.post("plantio/get_plantio_done/", params, {
+						headers: {
+							Authorization: `Token ${process.env.REACT_APP_DJANGO_TOKEN}`
+						}
+					})
+					.then((res) => {
+						// console.log(res.data);
+						const newData = res.data.data.map((data, i) => ({
+							...data,
+							id: i,
+							area_colheita: data.area_colheita
+								.toFixed(2)
+								.toString()
+								.replace(".", ","),
+							data_plantio: data.data_plantio
+								.split("-")
+								.reverse()
+								.join("/")
+						}));
+						setDataF(newData);
+					})
+					.catch((err) => console.log(err));
+			} catch (err) {
+				console.log("Erro ao consumir a API", err);
+			} finally {
+				console.log("Finally statement");
+				setIsLoading(false);
+			}
+		})();
+	}, []);
+
 	return (
 		<Box className={classes.container}>
-			<Typography variant="h1" color={colors.primary[200]}>
-				Dados de Área Plantada
-			</Typography>
+			{!dataF && <LoaderHomeSkeleton />}
+
+			{!isLoading && dataF && (
+				<PlantioDoneTable loading={isLoading} rows={dataF} />
+			)}
 		</Box>
 	);
 };
