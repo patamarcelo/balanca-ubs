15cefc0d8f589a4ea0d0db1450b97dc386fe43fb
patamarcelo <patamarcelo@gmail.com>
2023-08-02 08:20:59 -0300

    added badget selector

--- src/components/defensivos/farmbox-data/table-data-app-detail.jsx
+++ src/components/defensivos/farmbox-data/table-data-app-detail.jsx
@@ -1,5 +1,57 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import Chip from "@mui/material/Chip";
+import { Box } from "@mui/material";
+
+const colorDict = [
+	{
+		tipo: "Inseticida",
+		color: "rgb(218,78,75)"
+	},
+	{
+		tipo: "Herbicida",
+		color: "rgb(166,166,54)"
+	},
+	{
+		tipo: "Adjuvante",
+		color: "rgb(136,171,172)"
+	},
+	{
+		tipo: "Óleo",
+		color: "rgb(120,161,144)"
+	},
+	{
+		tipo: "Micronutrientes",
+		color: "rgb(118,192,226)"
+	},
+	{
+		tipo: "Fungicida",
+		color: "rgb(238,165,56)"
+	},
+	{
+		tipo: "Fertilizante",
+		color: "rgb(76,180,211)"
+	},
+	{
+		tipo: "Nutrição ",
+		color: "rgb(87,77,109)"
+	},
+	{
+		tipo: "Biológico",
+		color: "rgb(69,133,255)"
+	}
+];
+
+const getColorChip = (data) => {
+	console.log(data);
+	const folt = colorDict.filter((tipo) => tipo.tipo === data);
+	if (folt.length > 0) {
+		console.log(folt[0].color);
+		return folt[0].color;
+	} else {
+		return "rgb(255,255,255,0.1)";
+	}
+};
 
 const IconDetail = ({ color }) => {
 	return (
@@ -17,30 +69,53 @@ const IconDetail = ({ color }) => {
 const DetailAppData = ({ data, showData }) => {
 	return (
 		<>
-			<div>
+			<Box>
 				{data.parcelas.map((data, i) => {
 					return (
 						<>
 							<p>
 								{<IconDetail color={data.aplicado} />}
-								{data.parcela}{" "}
-								{data.area.toString().replace(".", ",")}
+								{data.parcela} -{" "}
+								{data.area
+									.toFixed(2)
+									.toString()
+									.replace(".", ",")}
 							</p>
 						</>
 					);
 				})}
-			</div>
+			</Box>
 			<div>
-				{data.insumos.map((data, i) => {
-					return (
-						<>
-							<p>
-								{data.dose} - {data.insumo} - {data.tipo} -{" "}
-								{data.quantidade}{" "}
-							</p>
-						</>
-					);
-				})}
+				{data.insumos
+					.sort((a, b) => a.tipo.localeCompare(b.tipo))
+					.map((data, i) => {
+						const tipo = data.tipo.includes("Óleo Mineral")
+							? "Óleo"
+							: data.tipo;
+						return (
+							<>
+								<p key={i}>
+									<b>{data.dose}</b>{" "}
+									<Chip
+										label={
+											tipo.includes("Óleo Mineral")
+												? "Óleo"
+												: tipo
+										}
+										sx={{
+											backgroundColor: getColorChip(tipo),
+											minWidth: "90px",
+											textAlign: "center",
+											height: "auto",
+											margin: "0px 8px 0px 4px"
+										}}
+										size="small"
+									/>
+									{data.insumo} - {data.quantidade}{" "}
+								</p>
+							</>
+						);
+					})}
 			</div>
 		</>
 	);
