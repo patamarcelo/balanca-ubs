d22c6b54bd5724fa4fd099bd710c878ccc6cb9d6
patamarcelo <patamarcelo@gmail.com>
2023-09-13 09:37:15 -0300

    css table home

--- src/components/home-itens/home-table-truck/index.jsx
+++ src/components/home-itens/home-table-truck/index.jsx
@@ -162,8 +162,13 @@ const HomeTableTruck = (props) => {
 							width: isNonMobile ? "98%" : "95%",
 							backgroundColor: colors.blueOrigin[800],
 							// border: `0.1px solid ${colors.primary[100]}`,
+							boxShadow: "rgba(0, 0, 0, 0.65) 0px 5px 15px",
 							borderRadius: "5px",
-							padding: "10px"
+							padding: "10px",
+							"&:hover": {
+								background: colors.blueOrigin[700],
+								// cursor: "pointer"
+							}
 						}}
 					>
 						<Box
