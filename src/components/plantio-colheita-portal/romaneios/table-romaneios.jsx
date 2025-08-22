b8267c339253497d2a3c28fa2c2518004842a17c
patamarcelo <patamarcelo@gmail.com>
2025-08-22 16:30:50 -0300

    update icon to - on romaneio table when has no obs

--- src/components/plantio-colheita-portal/romaneios/table-romaneios.jsx
+++ src/components/plantio-colheita-portal/romaneios/table-romaneios.jsx
@@ -347,7 +347,7 @@ const RomaneiosTable = (props) => {
 											</Tooltip>
 										) : (
 											<Box>
-												<InfoOutlinedIcon color="disabled" />
+												-
 											</Box>
 										)}
 									</td>
